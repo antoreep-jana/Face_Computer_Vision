cf636e256e009d5c082416729e586a69f88cff68
Antoreep Jana <antoreepjana@gmail.com>
2022-02-07 17:47:46 +0530

    Added Dropdown

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ st.title("Face Computer Vision")
 
 
 st.sidebar.markdown("""Perform various tasks related face using this tool""")
+
+
+#st.subheader("Choose an option")
+
+st.selectbox("Choose the operation to perform", ['Age Prediction', 'Face Deblur', 'Face Recognition', 'Sentiment Classification', "Mask Prediction"])
