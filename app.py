46c5e19b444e100122fe762584637aa91139acab
Antoreep Jana <antoreepjana@gmail.com>
2022-02-08 12:13:22 +0530

    Fixes

--- app.py
+++ app.py
@@ -15,34 +15,35 @@ option = st.selectbox("Choose the operation to perform", ['Age Prediction', 'Fac
 
 uploaded_file = st.file_uploader("Upload an image from local disk", type = ['jpg', 'jpeg', 'png'])
 
-	if uploaded_file is not None:
 
-		image = Image.open(uploaded_file)
+if uploaded_file is not None:
 
-		st.write("")
+	image = Image.open(uploaded_file)
+
+	st.write("")
 		
-		if option == 'Age Prediction':
+	if option == 'Age Prediction':
 
-			age = '10'
-			st.write(f"Predicted age : {age}")
+		age = '10'
+		st.write(f"Predicted age : {age}")
 
-		elif option == 'Face Deblur':
+	elif option == 'Face Deblur':
 
-			st.image(image, caption = 'Deblurred Image', use_column_width = True)
-			st.write("Face Deblurred")
+		st.image(image, caption = 'Deblurred Image', use_column_width = True)
+		st.write("Face Deblurred")
 
-		elif option == "Face Recognition":
+	elif option == "Face Recognition":
 
-			st.write("Recognizing face")
+		st.write("Recognizing face")
 
-		elif option == 'Sentiment Classification':
+	elif option == 'Sentiment Classification':
 
-			sentiment = 'happy'
-			st.write(f"Sentiment of the face : {sentiment}")
+		sentiment = 'happy'
+		st.write(f"Sentiment of the face : {sentiment}")
 
-		elif option == "Mask Prediction":
+	elif option == "Mask Prediction":
 
-			st.write("Predicted Mask")
+		st.write("Predicted Mask")
 
 
 
