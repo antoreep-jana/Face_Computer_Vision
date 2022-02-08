9a6ed12d3f63bcf3306d0bfe5a1db471a5bab7a8
Antoreep Jana <antoreepjana@gmail.com>
2022-02-08 11:49:28 +0530

    Image Upload button

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 
-
+from PIL import Image 
 
 st.title("Face Computer Vision")
 
@@ -10,4 +10,21 @@ st.sidebar.markdown("""Perform various tasks related face using this tool""")
 
 #st.subheader("Choose an option")
 
-st.selectbox("Choose the operation to perform", ['Age Prediction', 'Face Deblur', 'Face Recognition', 'Sentiment Classification', "Mask Prediction"])
+option = st.selectbox("Choose the operation to perform", ['Age Prediction', 'Face Deblur', 'Face Recognition', 'Sentiment Classification', "Mask Prediction"])
+
+
+if option == 'Age Prediction':
+
+    file = st.file_uploader("Upload an image from local disk", type = ['jpg', 'jpeg', 'png'])
+
+    if file is not None:
+
+        image = Image.open(file)
+
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.write("")
+        st.write("Classifying...")
+
+        label = 'Label1'
+        st.write(f"Classified as {label}")
+
